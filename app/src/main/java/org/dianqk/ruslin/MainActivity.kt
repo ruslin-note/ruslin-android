897eb218e663958aeb60b1170bf4ba918ea747b6
DianQK <dianqk@dianqk.net>
2022-12-09 21:17:19 +0800

    apply edge-to-edge

--- app/src/main/java/org/dianqk/ruslin/MainActivity.kt
+++ app/src/main/java/org/dianqk/ruslin/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import org.dianqk.ruslin.ui.theme.RuslinTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             RuslinTheme {
                 // A surface container using the 'background' color from the theme
--- app/src/main/java/org/dianqk/ruslin/ui/theme/Theme.kt
+++ app/src/main/java/org/dianqk/ruslin/ui/theme/Theme.kt
@@ -55,7 +55,6 @@ fun RuslinTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
             ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
         }
     }
