b17367cde340a9cc6e9daed99c1d1adaa54612bf
DianQK <dianqk@dianqk.net>
2022-12-24 15:20:47 +0800

    add autofill

--- app/src/main/java/org/dianqk/ruslin/ui/page/login/LoginPage.kt
+++ app/src/main/java/org/dianqk/ruslin/ui/page/login/LoginPage.kt
@@ -7,7 +7,15 @@ import androidx.compose.material.icons.outlined.Login
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.AutofillNode
+import androidx.compose.ui.autofill.AutofillType
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalAutofill
+import androidx.compose.ui.platform.LocalAutofillTree
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
@@ -20,7 +28,10 @@ import kotlinx.coroutines.launch
 import org.dianqk.ruslin.R
 import org.dianqk.ruslin.ui.component.FilledButtonWithIcon
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalLifecycleComposeApi::class)
+@OptIn(
+    ExperimentalMaterial3Api::class, ExperimentalLifecycleComposeApi::class,
+    ExperimentalComposeUiApi::class
+)
 @Composable
 fun LoginPage(
     viewModel: LoginViewModel = hiltViewModel(),
@@ -30,6 +41,7 @@ fun LoginPage(
     val scope = rememberCoroutineScope()
 
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val autofill = LocalAutofill.current
 
     Scaffold(
         modifier = Modifier.imePadding(),
@@ -68,35 +80,65 @@ fun LoginPage(
                         keyboardType = KeyboardType.Uri,
                     )
                 )
-                OutlinedTextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    value = uiState.email,
-                    label = {
-                        Text(text = stringResource(id = R.string.email))
-                    },
-                    singleLine = true,
-                    onValueChange = viewModel::setEmail,
-                    keyboardOptions = KeyboardOptions(
-                        capitalization = KeyboardCapitalization.None,
-                        autoCorrect = false,
-                        keyboardType = KeyboardType.Email,
+                Autofill(
+                    autofillTypes = listOf(AutofillType.EmailAddress),
+                    onFill = viewModel::setEmail,
+                ) { autofillNode ->
+                    OutlinedTextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .onFocusChanged { focusState ->
+                                autofill?.run {
+                                    if (focusState.isFocused) {
+                                        requestAutofillForNode(autofillNode)
+                                    } else {
+                                        cancelAutofillForNode(autofillNode)
+                                    }
+                                }
+                            },
+                        value = uiState.email,
+                        label = {
+                            Text(text = stringResource(id = R.string.email))
+                        },
+                        singleLine = true,
+                        onValueChange = viewModel::setEmail,
+                        keyboardOptions = KeyboardOptions(
+                            capitalization = KeyboardCapitalization.None,
+                            autoCorrect = false,
+                            keyboardType = KeyboardType.Email,
+                        )
                     )
-                )
-                OutlinedTextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    value = uiState.password,
-                    label = {
-                        Text(text = stringResource(id = R.string.password))
-                    },
-                    singleLine = true,
-                    visualTransformation = PasswordVisualTransformation(),
-                    onValueChange = viewModel::setPassword,
-                    keyboardOptions = KeyboardOptions(
-                        capitalization = KeyboardCapitalization.None,
-                        autoCorrect = false,
-                        keyboardType = KeyboardType.Password,
+                }
+                Autofill(
+                    autofillTypes = listOf(AutofillType.Password),
+                    onFill = viewModel::setPassword,
+                ) { autofillNode ->
+                    OutlinedTextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .onFocusChanged { focusState ->
+                                autofill?.run {
+                                    if (focusState.isFocused) {
+                                        requestAutofillForNode(autofillNode)
+                                    } else {
+                                        cancelAutofillForNode(autofillNode)
+                                    }
+                                }
+                            },
+                        value = uiState.password,
+                        label = {
+                            Text(text = stringResource(id = R.string.password))
+                        },
+                        singleLine = true,
+                        visualTransformation = PasswordVisualTransformation(),
+                        onValueChange = viewModel::setPassword,
+                        keyboardOptions = KeyboardOptions(
+                            capitalization = KeyboardCapitalization.None,
+                            autoCorrect = false,
+                            keyboardType = KeyboardType.Password,
+                        )
                     )
-                )
+                }
                 FilledButtonWithIcon(
                     modifier = Modifier
                         .padding(vertical = 12.dp)
@@ -117,5 +159,27 @@ fun LoginPage(
                 )
             }
         }
+    }
+}
+
+// https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:compose/ui/ui/integration-tests/ui-demos/src/main/java/androidx/compose/ui/demos/autofill/ExplicitAutofillTypesDemo.kt
+@ExperimentalComposeUiApi
+@Composable
+private fun Autofill(
+    autofillTypes: List<AutofillType>,
+    onFill: ((String) -> Unit),
+    content: @Composable (AutofillNode) -> Unit
+) {
+    val autofillNode = AutofillNode(onFill = onFill, autofillTypes = autofillTypes)
+
+    val autofillTree = LocalAutofillTree.current
+    autofillTree += autofillNode
+
+    Box(
+        Modifier.onGloballyPositioned {
+            autofillNode.boundingBox = it.boundsInWindow()
+        }
+    ) {
+        content(autofillNode)
     }
 }
