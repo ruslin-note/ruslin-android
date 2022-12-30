4d37316a29e4d4e8558bd807a812111707c5f16e
DianQK <dianqk@dianqk.net>
2022-12-30 22:56:38 +0800

    Build apks on github actions

--- ruslin-data-uniffi/build.sh
+++ ruslin-data-uniffi/build.sh
@@ -12,11 +12,26 @@ find -L $ANDROID_HOME/ndk/$ANDROID_NDK_VERSION -name libunwind.a -execdir sh -c 
 export ANDROID_NDK_TOOLCHAIN_BIN=$ANDROID_HOME/ndk/$ANDROID_NDK_VERSION/toolchains/llvm/prebuilt/linux-x86_64/bin
 
 export AR=$ANDROID_NDK_TOOLCHAIN_BIN/llvm-ar
+
+# export CC=$ANDROID_NDK_TOOLCHAIN_BIN/armv7a-linux-androideabi28-clang
+# export CXX=$ANDROID_NDK_TOOLCHAIN_BIN/armv7a-linux-androideabi28-clang++
+# export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER=$ANDROID_NDK_TOOLCHAIN_BIN/armv7a-linux-androideabi28-clang
+# cargo build --target armv7-linux-androideabi
+
+# export CC=$ANDROID_NDK_TOOLCHAIN_BIN/x86_64-linux-android28-clang
+# export CXX=$ANDROID_NDK_TOOLCHAIN_BIN/x86_64-linux-android28-clang++
+# export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER=$ANDROID_NDK_TOOLCHAIN_BIN/x86_64-linux-android28-clang
+# cargo build --target x86_64-linux-android
+
+# export CC=$ANDROID_NDK_TOOLCHAIN_BIN/i686-linux-android28-clang
+# export CXX=$ANDROID_NDK_TOOLCHAIN_BIN/i686-linux-android28-clang++
+# export CARGO_TARGET_I686_LINUX_ANDROID_LINKER=$ANDROID_NDK_TOOLCHAIN_BIN/i686-linux-android28-clang
+# cargo build --target i686-linux-android
+
 export CC=$ANDROID_NDK_TOOLCHAIN_BIN/aarch64-linux-android28-clang
 export CXX=$ANDROID_NDK_TOOLCHAIN_BIN/aarch64-linux-android28-clang++
-export CARGO_TARGET_AARCH64_LINUX_ANDROID_AR=$ANDROID_NDK_TOOLCHAIN_BIN/llvm-ar
+# export CARGO_TARGET_AARCH64_LINUX_ANDROID_AR=$ANDROID_NDK_TOOLCHAIN_BIN/llvm-ar
 export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$ANDROID_NDK_TOOLCHAIN_BIN/aarch64-linux-android28-clang
-
 cargo build --target aarch64-linux-android
 
 mkdir -p ../app/src/main/jniLibs/arm64-v8a
