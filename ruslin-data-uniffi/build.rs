0d02e6409ac5e10b7843095713c52c9b2cae53df
DianQK <dianqk@dianqk.net>
2023-01-06 22:47:39 +0800

    Fix cannot locate symbol __extenddftf2 referenced

--- ruslin-data-uniffi/build.rs
+++ ruslin-data-uniffi/build.rs
@@ -1,3 +1,16 @@
+use std::{env, path::Path, process::Command};
+
 fn main() {
+    let target = env::var("TARGET").unwrap();
+    if target == "x86_64-linux-android" || target == "i686-linux-android" {
+        let cc = env::var_os("CC").unwrap();
+        let cc = Path::new(&cc);
+        let output = Command::new(cc)
+            .arg("-print-libgcc-file-name")
+            .output()
+            .unwrap();
+        let rtlib_path = String::from_utf8(output.stdout).unwrap();
+        println!("cargo:rustc-link-arg={}", rtlib_path.trim()); // https://github.com/termux/termux-packages/issues/8029#issuecomment-1369150244
+    }
     uniffi_build::generate_scaffolding("./src/ruslin.udl").expect("generate_scaffolding error");
 }
