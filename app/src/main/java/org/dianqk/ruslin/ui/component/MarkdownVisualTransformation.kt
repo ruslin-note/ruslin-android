b4fe05108ed97fa01c7cf143bf04cdcde04a301a
DianQK <dianqk@dianqk.net>
2023-01-08 12:09:44 +0800

    Remove unused code

--- app/src/main/java/org/dianqk/ruslin/ui/component/MarkdownVisualTransformation.kt
+++ app/src/main/java/org/dianqk/ruslin/ui/component/MarkdownVisualTransformation.kt
@@ -1,19 +1,14 @@
 package org.dianqk.ruslin.ui.component
 
-import android.util.Log
 import androidx.compose.material3.Typography
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.BaselineShift
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.unit.sp
 import org.intellij.markdown.IElementType
 import org.intellij.markdown.MarkdownElementTypes
 import org.intellij.markdown.ast.ASTNode
@@ -23,15 +18,9 @@ import org.intellij.markdown.flavours.gfm.GFMElementTypes
 import org.intellij.markdown.flavours.gfm.GFMFlavourDescriptor
 import org.intellij.markdown.parser.MarkdownParser
 
-val HASHTAG_REGEX_PATTERN = Regex(pattern = "(#[A-Za-z0-9-_]+)(?:#[A-Za-z0-9-_]+)*")
-val BOLD_REGEX_PATTERN = Regex(pattern = "(\\*{2})(\\s*\\b)([^\\*]*)(\\b\\s*)(\\*{2})")
-val ITALICS_REGEX_PATTERN = Regex(pattern = "(\\~{2})(\\s*\\b)([^\\*]*)(\\b\\s*)(\\~{2})")
-val HEADING_REGEX_PATTERN = Regex(pattern = "\\#{1,4}\\s([^\\#]*)\\s\\#{1,4}(?=\\n)")
-
 class MarkdownVisualTransformation : VisualTransformation {
 
     override fun filter(text: AnnotatedString): TransformedText {
-        Log.d("MarkdownTF", "filter $text")
         val builder = AnnotatedString.Builder(text)
         val flavour = GFMFlavourDescriptor()
         val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(text.text)
@@ -48,7 +37,6 @@ class MarkdownVisualTransformation : VisualTransformation {
         private val providers: Map<IElementType, GeneratingProvider>
     ) : RecursiveVisitor() {
         override fun visitNode(node: ASTNode) {
-            Log.d("MarkdownTF", "$node -> ${node.type}")
             providers[node.type]?.processNode(this, builder, node) ?: node.acceptChildren(this)
         }
     }
@@ -185,116 +173,4 @@ class ATXHeaderGeneratingProvider(private val level: Int) : GeneratingProvider {
         }
         node.acceptChildren(visitor)
     }
-}
-
-fun transformHeading(text: AnnotatedString): AnnotatedString {
-    val matches = HEADING_REGEX_PATTERN.findAll(text.text)
-    return if (matches.count() > 0) {
-        Log.d("MarkdownTF", "match ${matches.count()}")
-        val builder = AnnotatedString.Builder(text)
-        for (match in matches) {
-            val matchRange = match.range
-            val headingLevel = getHeadingLevel(match.value)
-            val sizeList = listOf(32.sp, 28.sp, 24.sp, 18.sp)
-            builder.addStyle(
-                style = SpanStyle(
-                    color = Color.Gray,
-                    baselineShift = BaselineShift.Superscript,
-                    fontSize = sizeList[headingLevel - 1] / 4
-                ),
-                matchRange.first,
-                matchRange.first + headingLevel
-            )
-            builder.addStyle(
-                style = SpanStyle(
-                    fontWeight = FontWeight.Bold,
-                    fontSize = sizeList[headingLevel - 1]
-                ),
-                matchRange.first + headingLevel,
-                matchRange.last - headingLevel + 1
-            )
-            builder.addStyle(
-                style = SpanStyle(
-                    color = Color.Gray,
-                    baselineShift = BaselineShift.Superscript,
-                    fontSize = sizeList[headingLevel - 1] / 4
-                ),
-                matchRange.last - headingLevel + 1,
-                matchRange.last + 1
-            )
-        }
-        builder.toAnnotatedString()
-    } else {
-        text
-    }
-}
-
-// fun transformItalics(text: AnnotatedString): Transformation {
-//    val matches = ITALICS_REGEX_PATTERN.findAll(text.text)
-//    return if (matches.count() > 0) {
-//        val builder = AnnotatedString.Builder(text)
-//        for (match in matches) {
-//            val matchRange = match.range
-//            builder.addStyle(
-//                style = SpanStyle(color = Color.Gray, baselineShift = BaselineShift.Superscript, fontSize = 10.sp),
-//                matchRange.first,
-//                matchRange.first + 2
-//            )
-//            builder.addStyle(style = SpanStyle(fontStyle = FontStyle.Italic), matchRange.first + 2, matchRange.last - 1)
-//            builder.addStyle(
-//                style = SpanStyle(color = Color.Gray, baselineShift = BaselineShift.Superscript, fontSize = 10.sp),
-//                matchRange.last - 1,
-//                matchRange.last + 1
-//            )
-//        }
-//        Transformation(annotatedString = builder.toAnnotatedString(), offsetMapping = OffsetMapping.Identity)
-//    } else {
-//        Transformation(annotatedString = text, offsetMapping = OffsetMapping.Identity)
-//    }
-// }
-//
-// fun transformBold(text: AnnotatedString): Transformation {
-//    val matches = BOLD_REGEX_PATTERN.findAll(text.text)
-//    return if (matches.count() > 0) {
-//        val builder = AnnotatedString.Builder(text)
-//        for (match in matches) {
-//            val matchRange = match.range
-//            builder.addStyle(
-//                style = SpanStyle(color = Color.Gray, baselineShift = BaselineShift.Superscript, fontSize = 10.sp),
-//                matchRange.first,
-//                matchRange.first + 2
-//            )
-//            builder.addStyle(style = SpanStyle(fontWeight = FontWeight.Bold), matchRange.first + 2, matchRange.last - 1)
-//            builder.addStyle(
-//                style = SpanStyle(color = Color.Gray, baselineShift = BaselineShift.Superscript, fontSize = 10.sp),
-//                matchRange.last - 1,
-//                matchRange.last + 1
-//            )
-//        }
-//        Transformation(annotatedString = builder.toAnnotatedString(), offsetMapping = OffsetMapping.Identity)
-//    } else {
-//        Transformation(annotatedString = text, offsetMapping = OffsetMapping.Identity)
-//    }
-// }
-//
-// fun transformHashtags(text: AnnotatedString): Transformation {
-//    val builder = AnnotatedString.Builder(text)
-//    val matches = HASHTAG_REGEX_PATTERN.findAll(text.text)
-//    for (match in matches) {
-//        val matchRange = match.range
-//        builder.addStyle(style = SpanStyle(color = Color.Yellow), start = matchRange.first, end = matchRange.last + 1)
-//    }
-//    return Transformation(annotatedString = builder.toAnnotatedString(), offsetMapping = OffsetMapping.Identity)
-// }
-
-private fun getHeadingLevel(text: String): Int {
-    var i = 0
-    while (i < text.length) {
-        if (text[i] == '#') {
-            i++
-        } else {
-            break
-        }
-    }
-    return i
 }
