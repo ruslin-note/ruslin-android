567bd589044e32bcd88b235a2e0fed7b4c2375b8
DianQK <dianqk@dianqk.net>
2023-02-04 09:38:32 +0800

    Remove uniffi-bindgen.rs

--- ruslin-data-uniffi/release.sh
+++ ruslin-data-uniffi/release.sh
@@ -19,5 +19,3 @@ cargo build --target aarch64-linux-android --release
 
 mkdir -p ../uniffi/src/main/jniLibs/arm64-v8a
 cp target/aarch64-linux-android/release/libuniffi_ruslin.so ../uniffi/src/main/jniLibs/arm64-v8a
-
-cargo run --target $(rustc -vV | awk '/^host/ { print $2 }') --features=uniffi/cli --bin uniffi-bindgen generate src/ruslin.udl --language kotlin --out-dir ../uniffi/src/main/java
