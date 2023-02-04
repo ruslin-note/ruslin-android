e80bd0b837c77571f1a9249e15e4e7196c89e68c
DianQK <dianqk@dianqk.net>
2023-02-04 18:56:29 +0800

    Update release.sh

--- ruslin-data-uniffi/release.sh
+++ ruslin-data-uniffi/release.sh
@@ -2,20 +2,47 @@
 
 set -e
 
-export ANDROID_HOME=$HOME/Android/Sdk
 export ANDROID_NDK_VERSION=25.1.8937393
 
 find -L $ANDROID_HOME/ndk/$ANDROID_NDK_VERSION -name libunwind.a -execdir sh -c 'echo "INPUT(-lunwind)" > libgcc.a' \;
 
-export ANDROID_NDK_TOOLCHAIN_BIN=$ANDROID_HOME/ndk/$ANDROID_NDK_VERSION/toolchains/llvm/prebuilt/linux-x86_64/bin
-
+ANDROID_NDK_TOOLCHAIN_BIN=$ANDROID_HOME/ndk/$ANDROID_NDK_VERSION/toolchains/llvm/prebuilt/linux-x86_64/bin
 export AR=$ANDROID_NDK_TOOLCHAIN_BIN/llvm-ar
-export CC=$ANDROID_NDK_TOOLCHAIN_BIN/aarch64-linux-android28-clang
-export CXX=$ANDROID_NDK_TOOLCHAIN_BIN/aarch64-linux-android28-clang++
-export CARGO_TARGET_AARCH64_LINUX_ANDROID_AR=$ANDROID_NDK_TOOLCHAIN_BIN/llvm-ar
-export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$ANDROID_NDK_TOOLCHAIN_BIN/aarch64-linux-android28-clang
 
-cargo build --target aarch64-linux-android --release
+ANDROID_ABI=$1
 
-mkdir -p ../uniffi/src/main/jniLibs/arm64-v8a
-cp target/aarch64-linux-android/release/libuniffi_ruslin.so ../uniffi/src/main/jniLibs/arm64-v8a
+case "$ANDROID_ABI" in
+arm64-v8a)
+    RUST_TARGET="aarch64-linux-android"
+    export CC=$ANDROID_NDK_TOOLCHAIN_BIN/aarch64-linux-android28-clang
+    export CXX=$ANDROID_NDK_TOOLCHAIN_BIN/aarch64-linux-android28-clang++
+    export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$ANDROID_NDK_TOOLCHAIN_BIN/aarch64-linux-android28-clang
+    ;;
+armeabi-v7a)
+    RUST_TARGET="armv7-linux-androideabi"
+    export CC=$ANDROID_NDK_TOOLCHAIN_BIN/armv7a-linux-androideabi28-clang
+    export CXX=$ANDROID_NDK_TOOLCHAIN_BIN/armv7a-linux-androideabi28-clang++
+    export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER=$ANDROID_NDK_TOOLCHAIN_BIN/armv7a-linux-androideabi28-clang
+    ;;
+x86_64)
+    RUST_TARGET="x86_64-linux-android"
+    export CC=$ANDROID_NDK_TOOLCHAIN_BIN/x86_64-linux-android28-clang
+    export CXX=$ANDROID_NDK_TOOLCHAIN_BIN/x86_64-linux-android28-clang++
+    export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER=$ANDROID_NDK_TOOLCHAIN_BIN/x86_64-linux-android28-clang
+    ;;
+i686)
+    RUST_TARGET="i686-linux-android"
+    export CC=$ANDROID_NDK_TOOLCHAIN_BIN/i686-linux-android28-clang
+    export CXX=$ANDROID_NDK_TOOLCHAIN_BIN/i686-linux-android28-clang++
+    export CARGO_TARGET_I686_LINUX_ANDROID_LINKER=$ANDROID_NDK_TOOLCHAIN_BIN/i686-linux-android28-clang
+    ;;
+*)
+    echo "Unsupported"
+    exit 1
+esac
+
+echo "Rust target: $RUST_TARGET"
+
+cargo build --target $RUST_TARGET --verbose --release
+mkdir -p ../uniffi/src/main/jniLibs/$ANDROID_ABI
+cp target/$RUST_TARGET/release/libuniffi_ruslin.so ../uniffi/src/main/jniLibs/$ANDROID_ABI
