276db0d8dc94517a4e606807ba06c2cd5cf0f3f5
DianQK <dianqk@dianqk.net>
2023-02-26 22:08:26 +0800

    Bum gradle to 8.0.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,12 @@ pluginManagement {
         mavenCentral()
     }
 }
+
+plugins {
+    // https://docs.gradle.org/8.0.1/userguide/toolchains.html#sub:download_repositories
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
