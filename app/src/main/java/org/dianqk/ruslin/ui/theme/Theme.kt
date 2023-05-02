8e6b25070834863b7b5e2ace9c51fe8f92c354d2
DianQK <dianqk@dianqk.net>
2023-05-02 21:27:01 +0800

    Fix the web preview does not match the theme after setting the dark theme manually

--- app/src/main/java/org/dianqk/ruslin/ui/theme/Theme.kt
+++ app/src/main/java/org/dianqk/ruslin/ui/theme/Theme.kt
@@ -3,16 +3,21 @@ package org.dianqk.ruslin.ui.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import org.dianqk.ruslin.R
 import org.dianqk.ruslin.data.LocalDarkTheme
 import org.dianqk.ruslin.data.LocalThemeIndex
+import org.dianqk.ruslin.data.preference.DarkThemePreference
 import org.dianqk.ruslin.ui.theme.palette.LocalTonalPalettes
 import org.dianqk.ruslin.ui.theme.palette.TonalPalettes
 import org.dianqk.ruslin.ui.theme.palette.core.ProvideZcamViewingConditions
 import org.dianqk.ruslin.ui.theme.palette.dynamic.extractTonalPalettesFromUserWallpaper
 import org.dianqk.ruslin.ui.theme.palette.dynamicDarkColorScheme
 import org.dianqk.ruslin.ui.theme.palette.dynamicLightColorScheme
+
 
 fun Color.applyOpacity(enabled: Boolean): Color {
     return if (enabled) this else this.copy(alpha = 0.62f)
@@ -23,6 +28,7 @@ fun RuslinTheme(
     wallpaperPalettes: List<TonalPalettes> = extractTonalPalettesFromUserWallpaper(),
     content: @Composable () -> Unit
 ) {
+    val context = LocalContext.current
     val darkTheme = LocalDarkTheme.current
     val themeIndex = LocalThemeIndex.current
     val useDarkTheme = darkTheme.isDarkTheme()
@@ -39,6 +45,16 @@ fun RuslinTheme(
         setStatusBarColor(Color.Transparent, !useDarkTheme)
         setSystemBarsColor(Color.Transparent, !useDarkTheme)
         setNavigationBarColor(Color.Transparent, !useDarkTheme)
+    }
+
+    LaunchedEffect(darkTheme) {
+        context.setTheme(
+            when (darkTheme) {
+                DarkThemePreference.UseDeviceTheme -> R.style.Theme_Ruslin
+                DarkThemePreference.ON -> R.style.Theme_Ruslin_Dark
+                DarkThemePreference.OFF -> R.style.Theme_Ruslin_Light
+            }
+        )
     }
 
     ProvideZcamViewingConditions {
