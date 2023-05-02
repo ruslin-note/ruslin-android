cf2c08b441d65a11179481e42020ae1aa6e382c0
DianQK <dianqk@dianqk.net>
2023-05-03 06:30:12 +0800

    Bump jvm to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,18 +103,18 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
         freeCompilerArgs = freeCompilerArgs + "-opt-in=kotlin.RequiresOptIn"
         freeCompilerArgs = freeCompilerArgs + "-Xjvm-default=all"
     }
 
     kotlin {
-        jvmToolchain(11)
+        jvmToolchain(17)
     }
 
     buildFeatures {
--- mdrender/build.gradle.kts
+++ mdrender/build.gradle.kts
@@ -25,12 +25,15 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = JavaVersion.VERSION_17.toString()
 //        freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn -Xjvm-default=all"
+    }
+    kotlin {
+        jvmToolchain(17)
     }
 }
 
