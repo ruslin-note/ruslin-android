8fb6da7f52d38826a7be72bc314788a9f3ec6445
DianQK <dianqk@dianqk.net>
2023-05-09 22:01:50 +0800

    Remove confusing RTL options

--- app/src/main/java/org/dianqk/ruslin/data/preference/TextDirectionPreference.kt
+++ app/src/main/java/org/dianqk/ruslin/data/preference/TextDirectionPreference.kt
@@ -6,15 +6,14 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import org.dianqk.ruslin.R
 import org.dianqk.ruslin.data.DataStoreKeys
 import org.dianqk.ruslin.data.dataStore
 
 sealed class TextDirectionPreference(val value: Int) {
     object Ltr : TextDirectionPreference(1)
     object Rtl : TextDirectionPreference(2)
-    object Content : TextDirectionPreference(3)
-    object ContentOrLtr : TextDirectionPreference(4)
-    object ContentOrRtl : TextDirectionPreference(5)
+    object Auto : TextDirectionPreference(3)
 
     fun put(context: Context, scope: CoroutineScope) {
         scope.launch {
@@ -24,14 +23,16 @@ sealed class TextDirectionPreference(val value: Int) {
         }
     }
 
-    fun toDesc(context: Context): String = getTextDirection().toString()
+    fun toDesc(context: Context): String = when (this) {
+        Ltr -> context.getString(R.string.ltr)
+        Rtl -> context.getString(R.string.rtl)
+        Auto -> context.getString(R.string.auto)
+    }
 
     fun getTextDirection(): TextDirection = when (this) {
         Ltr -> TextDirection.Ltr
         Rtl -> TextDirection.Rtl
-        Content -> TextDirection.Content
-        ContentOrLtr -> TextDirection.ContentOrLtr
-        ContentOrRtl -> TextDirection.ContentOrRtl
+        Auto -> TextDirection.Content
     }
 
     companion object {
@@ -39,18 +40,14 @@ sealed class TextDirectionPreference(val value: Int) {
         val values = listOf(
             Ltr,
             Rtl,
-            Content,
-            ContentOrLtr,
-            ContentOrRtl,
+            Auto
         )
 
         fun fromPreferences(preferences: Preferences): TextDirectionPreference =
             when (preferences[DataStoreKeys.TextDirection.key]) {
                 1 -> Ltr
                 2 -> Rtl
-                3 -> Content
-                4 -> ContentOrLtr
-                5 -> ContentOrRtl
+                3 -> Auto
                 else -> default
             }
     }
