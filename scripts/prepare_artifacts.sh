d922e10448a93b701b73bcf62402088fee7524c0
DianQK <dianqk@dianqk.net>
2024-09-05 18:43:33 +0800

    Update scripts/prepare_artifacts.sh

--- scripts/prepare_artifacts.sh
+++ scripts/prepare_artifacts.sh
@@ -6,7 +6,7 @@ pushd app/build/outputs/mapping/release
 zip mapping.zip *.txt
 popd
 
-find app/build
 mv app/build/outputs/bundle/release/app-release.aab app/build/outputs/bundle/release/x-app-release.aab
 mv app/build/outputs/mapping/release/mapping.zip app/build/outputs/mapping/release/x-mapping.zip
-mv app/build/outputs/native-debug-symbols/release/native-debug-symbols.zip app/build/outputs/native-debug-symbols/release/x-native-debug-symbols.zip
+# FIXME: Unable to retrieve this in CI.
+# mv app/build/outputs/native-debug-symbols/release/native-debug-symbols.zip app/build/outputs/native-debug-symbols/release/x-native-debug-symbols.zip
