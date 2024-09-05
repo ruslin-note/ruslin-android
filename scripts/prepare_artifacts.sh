860e1ff2433fa3f38ea0da689cd299337a5ebf67
DianQK <dianqk@dianqk.net>
2024-09-05 09:12:44 +0800

    Update scripts/prepare_artifacts.sh

--- scripts/prepare_artifacts.sh
+++ scripts/prepare_artifacts.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
@@ -6,6 +6,7 @@ pushd app/build/outputs/mapping/release
 zip mapping.zip *.txt
 popd
 
+find app/build
 mv app/build/outputs/bundle/release/app-release.aab app/build/outputs/bundle/release/x-app-release.aab
 mv app/build/outputs/mapping/release/mapping.zip app/build/outputs/mapping/release/x-mapping.zip
 mv app/build/outputs/native-debug-symbols/release/native-debug-symbols.zip app/build/outputs/native-debug-symbols/release/x-native-debug-symbols.zip
